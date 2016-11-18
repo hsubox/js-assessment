482c3f2eb37123b84841130d59e8fe317d2e9602
Jennifer Hsu <jennifer.y.hsu@gmail.com>
2016-11-18 15:00:42 -0500

    completed all exercises

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(function (resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise(function (resolve, reject) {
+      var httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+            var response = JSON.parse(httpRequest.responseText).people.map((person) => person.name).sort();
+            resolve(response);
+          } else {
+            reject('There was a problem with the request.');
+          }
+        }
+      };
+      httpRequest.open('GET', url, true);
+      httpRequest.send();
+    });
   }
 };
--- app/count.js
+++ app/count.js
@@ -2,6 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    var cancel = function() {
+      window.clearInterval(exports.countAnswers.counter);
+    };
+    var current = start;
+    console.log(current);
+    exports.countAnswers.counter = window.setInterval(function() {
+      current++;
+      console.log(current);
+      if (current >= end) {
+        cancel();
+      }
+    }, 100);
+    return {cancel};
   }
 };
